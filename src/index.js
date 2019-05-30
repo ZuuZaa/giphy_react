27f1131d089b8a295e416032c1e64109b591337e
ZuuZaa <zumbaghirova@gmail.com>
2019-05-30 18:29:38 +0400

    commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let gifs = []
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+axios.get('https://api.giphy.com/v1/gifs/search?api_key=O5LyiGH753b9EWs8TEN6LL0DLtx8wqNm&q=cat&limit=25&offset=0&rating=G&lang=en').then((response) => {
+
+    gifs = response.data.data.map((value, index) => {
+        return (
+            <div className='col-4 p-2'>
+                <div className='card'>
+                    <img src={value.images['480w_still'].url} className="img-thumbnail"/>
+                </div>
+            </div>
+            )
+    })
+
+    ReactDOM.render(<div className='row'>{gifs}</div>, document.getElementById('root'));
+})
+
+
+
+
+
